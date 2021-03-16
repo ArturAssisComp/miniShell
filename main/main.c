b36fce6bfaa0ddf63ffb4e737a6ebe90d25b50ce
ArturAssisComp <arturassiscomp@gmail.com>
2021-03-16 10:56:07 -0300

    Implement the first draft of the shell

--- main/main.c
+++ main/main.c
@@ -15,20 +15,40 @@
 #define MAX_SIZE  1000 
 #define MAX_WORDS 100
 
-int main()
-{
-	char *buffer, **words;
-	int num_of_args;
-	buffer = (char *)xcalloc(MAX_SIZE + 1, sizeof (char));
-	buffer = (char **)xcalloc(MAX_WORDS, sizeof (char *));
-	
-	while(fgets(buffer, MAX_SIZE, stdin))
+int main(){
+	token **my_array;
+	int i;
+	char line[MAX_SIZE + 1];
+
+	//Initialize the shell:
+	printf("\nYourShell!\n");
+	printf(">>> ");
+	while(fgets(line, MAX_SIZE, stdin))
 	{
-		num_of_args = get_words(words, buffer);
-		int i = 0;
-		for (; i < num_of_args; i++)
-			printf("Word[%d] = '%s'\n", i + 1, words[i]);
+		my_array = get_tokens(line);
+		if(my_array == NULL)
+		{
+			printf("\n>>> ");
+		}
+		else
+		{
+			i = 0;
+			while(my_array[i])
+			{
+				printf("--> my_array[%d]->string = %s\n", i, my_array[i]->string);
+				i++;
+			}
+			printf("\n>>> ");
+		}
 	}
-	free(buffer);
+
+	//Free the allocated memory:
+	i = 0;
+	while (my_array[i])
+	{
+		if (my_array[i]->string)
+			free(my_array[i]->string);
+	}
+	free(my_array);
 	return 0;
 }
