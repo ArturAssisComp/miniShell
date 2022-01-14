9e235b608589df0a184574f3ddedfcfaca483edd
Artur Assis Alves <arturassiscomp@gmail.com>
2022-01-14 14:42:27 -0300

    Add tag to store the size of memory allocated.

--- src/aux/shared_alloc.c
+++ src/aux/shared_alloc.c
@@ -1,17 +1,20 @@
 #include "shared_alloc.h"
-#include <sys/mmap.h>
+#include <sys/mman.h>
+#include <stddef.h>
 
 void *SA_malloc(size_t size)
 {
     void *addr;
-    addr = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    addr = mmap(NULL, sizeof(size_t) + size, PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     if(addr == MAP_FAILED) addr = NULL;
-    return addr;
+    else ((size_t *) addr)[0] = size;
+    return &((size_t *)addr)[1];
 }
 
 
-void SA_free(void *ptr, size_t size)
+void SA_free(void *ptr)
 {
+    size_t size = ((size_t *)ptr)[-1];
     munmap(ptr, size);
 }
 
@@ -20,5 +23,6 @@ void *SA_calloc(size_t nmemb, size_t size)
     void *addr;
     addr = mmap(NULL, size * nmemb, PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     if(addr == MAP_FAILED) addr = NULL;
-    return addr;
+    else ((size_t *) addr)[0] = size * nmemb;
+    return &((size_t *)addr)[1];
 }
