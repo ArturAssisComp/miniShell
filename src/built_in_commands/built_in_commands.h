8f6a3efe1ec4664ceba57e2dfeae82b4bd34750b
Artur Assis Alves <arturassiscomp@gmail.com>
2022-01-24 21:45:24 -0300

    Implement cd and exit built in functions

--- src/built_in_commands/built_in_commands.h
+++ src/built_in_commands/built_in_commands.h
@@ -3,6 +3,10 @@
 #include <stdbool.h>
 
 bool BIC_is_built_in_command(const char id[]);
+int BIC_exec_built_in_cmd(const char *built_in_cmd_id, char *const argv[], char *const envp[]);
+void BIC_init_built_in_functions(void);
+void BIC_finish_built_in_functions(void);
+
 
 
 #endif
